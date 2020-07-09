348a5f1d893d9137885bd15702ec20c38f0ec91e
johnnytortorici <johnny.tortorici@gmail.com>
2020-07-09 12:56:09 -0400

    Exercise 4.1-4.2

--- handlers/handlers.js
+++ handlers/handlers.js
@@ -6,27 +6,27 @@ let currentUser = {};
 
 // declare the homepage handler
 const handleHomepage = (req, res) => {
-    res.status(200).render('pages/homepage', { users: users });
+    res.status(200).render('pages/homepage', { users: users, currentUser: currentUser });
 };
 
 // declare the profile page handler
 const handleProfilePage = (req, res) => {
     const _id = req.params._id;
     // find currentUser by _id
-    currentUser = users.find( (user) => {
+    let user = users.find( (user) => {
         return user._id === _id;
     });
     // only render profile page if user exists with that _id
-    if (currentUser !== undefined) {
+    if (user !== undefined) {
         // find userFriends by _id
         let userFriends = [];
-        currentUser.friends.forEach( (friend) => {
+        user.friends.forEach( (friend) => {
             users.forEach( (user) => {
                 if (user._id === friend) userFriends.push(user);
             });
         });
         // render profile page and pass currentUser object and friends array
-        res.status(200).render('pages/profile', { user: currentUser, friends: userFriends });
+        res.status(200).render('pages/profile', { user: user, friends: userFriends, currentUser: currentUser });
     } else {
         // if _id does not exist, redirect to home page
         res.status(404).redirect('/');
@@ -35,7 +35,7 @@ const handleProfilePage = (req, res) => {
 
 // declare the signin page handler
 const handleSignin = (req, res) => {
-    res.status(200).render('pages/signin');
+    res.status(200).render('pages/signin', { currentUser: currentUser });
 };
 
 // declare name handler for signin form
@@ -46,6 +46,7 @@ const handleName = (req, res) => {
     });
     // redirect to profile page only if login matches user name
     if (userLogin !== undefined) {
+        currentUser = userLogin;
         res.status(200).redirect(`/users/${userLogin._id}`);
     } else {
         res.status(404).redirect('/signin');
